f7a9100b8e64f95e8669893b93d9061c7a3fcdc6
Juan <juandiegovelsol@gmail.com>
2023-09-18 17:27:27 -0500

    Feat: header desktop

--- src/components/UpperBar/UpperBar.jsx
+++ src/components/UpperBar/UpperBar.jsx
@@ -1,7 +1,47 @@
 import "./upper-bar.scss";
+import starbust from "../../assets/fluent_checkmark-starburst-20-regular.svg";
+import sync from "../../assets/fluent_arrow-sync-checkmark-20-regular.svg";
+import heart from "../../assets/mdi_cards-heart-outline.svg";
+import truck from "../../assets/ph_truck-light.svg";
+import arrow from "../../assets/Layer_1.svg";
+
+const content = [
+  {
+    image: starbust,
+    text: "30-DAY SATISFACTION GUARANTEE",
+  },
+  {
+    image: truck,
+    text: "FREE DELIVERY ON ORDERS OVER $40.00",
+  },
+  {
+    image: heart,
+    text: "50.000+ HAPPY CUSTOMERS",
+  },
+  {
+    image: sync,
+    text: "100% MONEY BACK GUARANTEE",
+  },
+];
 
 const UpperBar = () => {
-  return <></>;
+  return (
+    <header className="upper-bar">
+      <img src={arrow} className="upper-bar__arrow" alt="arrow" />
+      {content.length &&
+        content.map(({ image, text }) => (
+          <section key={text} className="upper-bar__section">
+            <img
+              src={image}
+              alt="section"
+              className="upper-bar__section-image"
+            />
+            <p className="upper-bar__section-text">{text}</p>
+          </section>
+        ))}
+      <img src={arrow} className="upper-bar__arrow" alt="arrow" />
+    </header>
+  );
 };
 
 export default UpperBar;
