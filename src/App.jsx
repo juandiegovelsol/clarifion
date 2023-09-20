bec45090d65821641658b8aacbf62f06e9bf7912
Juan <juandiegovelsol@gmail.com>
2023-09-19 23:16:07 -0500

    Feat: mobile layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
 import { UpperBar } from "./components/UpperBar";
 import { Title } from "./components/Title";
 import { Titletext } from "./components/Titletext";
 import { ProcessBar } from "./components/PorcessBar";
 import { Container } from "./components/Container";
 import { Footer } from "./components/Footer";
-/* import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg"; */
+
 import "./App.scss";
 
 function App() {
-  /* const [count, setCount] = useState(0); */
-
   return (
     <>
       <div className="app">
